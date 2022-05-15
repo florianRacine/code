bab367122438e11cea6eeaa39e3145349943ef41
Florian <florian.racine@centrale-marseille.fr>
2022-05-15 10:52:39 +0200

    Test new macro vim

---
+++ cpp/counting.dir/counting.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+typedef long long ll;
+ 
+int32_t main() {
+    int n = 10;
+    for (int i = 0; i <= n; i++) {
+        std::cout << i << std::endl;
+    }
+}
