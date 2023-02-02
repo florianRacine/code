5275e434be95dd09f6df66f16a1a83c9161942bd
florianRacine <florian.racine@centrale-marseille.fr>
2023-02-02 13:25:54 +0100

    hammingCode

---
+++ cpp/hammingCode.dir/hammingCode.cpp
@@ -0,0 +1,18 @@
+/**
+ *    author:  evoPrg
+ *    created: 13.01.2023 18:43:46
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // Process input
+    int n, m;
+    cin >> n >> m;
+    vector<int> v(n*m);
+    for (int i = 0; i < n*m; i++) {
+            cin >> v[i];
+    }
+    return 0;
+}
