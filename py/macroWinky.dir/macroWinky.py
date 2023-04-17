d1932a61b5e384c08172f0741eb6572a074c4150
Florian <florian.racine@centrale-marseille.fr>
2023-04-17 19:05:10 +0200

    newPrg75ladder

--- py/macroWinky.dir/macroWinky.py
+++ py/macroWinky.dir/macroWinky.py
@@ -10,75 +10,85 @@ pyperclip.copy("@")
 with open("inp", "r") as filin:
     email = filin.readline()
     while email != "":
+        print(email)
         email = filin.readline()
+        time.sleep(0.07);
         # You can define what you want to search
         find = 'Winkyverse'
 
         # Use keys below
         pyautogui.hotkey('ctrl', 'f')
+        time.sleep(0.07);
 
-        pyautogui.write(find, interval=0.03)
+        pyautogui.write(find, interval=0.06)
+        time.sleep(0.07);
 
         pyautogui.press('escape')
-
-        pyautogui.press('enter')
-
-        time.sleep(0.25);
-
-        pyautogui.press('tab')
-        pyautogui.press('tab')
-
-        pyautogui.press('enter')
-
-        time.sleep(0.3);
-        pyautogui.write(email, interval=0.04)
         time.sleep(0.07);
+
+        pyautogui.press('enter')
+
+        time.sleep(0.6);
+
+        pyautogui.press('tab')
+        time.sleep(0.06);
+        pyautogui.press('tab')
+        time.sleep(0.06);
+
+        pyautogui.press('enter')
+
+        time.sleep(0.6);
+        pyautogui.write(email, interval=0.05)
+        time.sleep(0.1);
         pyperclip.copy("@")
-        pyautogui.hotkey('ctrl', 'v')
         time.sleep(0.07);
-        pyautogui.write("grr", interval=0.04)
+        pyautogui.hotkey('ctrl', 'v')
+        time.sleep(0.1);
+        pyautogui.write("gmail", interval=0.07)
+        time.sleep(0.07);
         pyperclip.copy(".")
+        time.sleep(0.07);
         pyautogui.hotkey('ctrl', 'v')
-        time.sleep(0.07);
-        pyautogui.write("la", interval=0.04)
-        time.sleep(0.07);
+        time.sleep(0.1);
+        pyautogui.write("com", interval=0.06)
+        time.sleep(0.1);
         pyautogui.press('tab')
-        time.sleep(0.05);
+        time.sleep(0.07);
         pyperclip.copy("1227827387233456")
         pyautogui.hotkey('ctrl', 'v')
-        time.sleep(0.05);
+        time.sleep(0.07);
         pyautogui.press('tab')
-        time.sleep(0.05);
+        time.sleep(0.07);
         pyautogui.hotkey('ctrl', 'v')
-        time.sleep(0.05);
+        time.sleep(0.07);
         pyautogui.moveTo(980, 580)
-        time.sleep(0.05);
+        time.sleep(0.1);
         pyautogui.click()
-        time.sleep(0.05);
+        time.sleep(0.1);
         pyautogui.moveTo(1080, 650)
-        time.sleep(0.05);
+        time.sleep(0.1);
         pyautogui.click()
-        time.sleep(0.3);
+        time.sleep(0.5);
         pyautogui.press('escape')
-        time.sleep(0.06);
+        time.sleep(0.1);
         pyautogui.press('escape')
-        time.sleep(0.4);
+        time.sleep(0.6);
         # You can define what you want to search
         find = 'My account'
 
         # Use keys below
         pyautogui.hotkey('ctrl', 'f')
 
-        pyautogui.write(find, interval=0.04)
-        time.sleep(0.03);
+        pyautogui.write(find, interval=0.06)
+        time.sleep(0.07);
         pyautogui.press('escape')
-        time.sleep(0.03);
+        time.sleep(0.07);
         pyautogui.press('enter')
-        time.sleep(0.03);
+        time.sleep(0.07);
         pyautogui.press('tab')
-        time.sleep(0.03);
+        time.sleep(0.07);
         pyautogui.press('tab')
-        time.sleep(0.03);
+        time.sleep(0.07);
         pyautogui.press('enter')
-        time.sleep(0.5);
+        time.sleep(0.8);
 filin.close()
