7f2a0de09fc77982261b98b9bf451f22d70d57cc
florianRacine <florian.racine@centrale-marseille.fr>
2023-09-26 14:43:23 +0200

    newPrg

---
+++ cpp/sequenceGame.dir/sequenceGame.cpp
@@ -0,0 +1,15 @@
+/**
+ *    author:  evoPrg
+ *    created: 26.09.2023 02:12:05
+**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int tt; cin >> tt;
+    while (tt--) {
+        
+    }
+    return 0;
+}
